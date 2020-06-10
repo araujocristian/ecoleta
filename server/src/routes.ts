5b8a537592aff1f51b4e5affcc42bf791fe76523
Cristian Araujo <cristian.jesus@zup.com.br>
2020-06-09 23:44:42 -0300

    Finalizando server

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,35 +1,40 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
 
-const routes = express.Router();
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World'});
-})
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const routes = express.Router(); 
+const upload = multer(multerConfig);
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+routes.post(
+  '/points', 
+  upload.single('image'), 
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false
+  }),
+  pointsController.create
+);
 
 export default routes;
-
-// routes.get('/users', (request, response) => {
-//   const search = String(request.query.search);
-
-//   const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-  
-//   return response.json(filteredUsers);
-// })
-
-// routes.get('/users/:id', (request, response) => {
-//   const id  = Number(request.params.id);
-
-//   const user = users[id];
-//   return response.json(user);
-// })
-
-// routes.post("/users", (request, response)=>{
-//   const data = request.body;
-
-//   const user = {
-//     name: data.name,
-//     email: data.email,
-//   };
-
-//   return response.json(user);
-// })
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-// Rota:  Endereço completo da requisição
-// Recurso: Qual entidade estamos acessando do sistema
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-// Verbos HTTP
-// GET: Buscar uma ou mais informações do back-end
-// POST: Criar uma nova informação no back-end
-// PUT: Atualizar uma informação existente no back-end
-// DELETE: Remover uma informação do back-end
-// Exemplos:
-// POST http://localhost:3333/users = Criar um usuário
-// GET http://localhost:3333/users = Listar usuários
-// GET http://localhost:3333/users/5 = Buscar dados de um usuário especifico
-
-// Tipos de parâmetros
-// Request Params: Parametros que vem na propria rota que identificam um recurso
-// Query Params: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação
-// Request Body: Parâmetros no corpo da requisição para criar/atualizar informações
-
-// Banco de dados
-// SELECT = FROM users WHERE name = 'Cristian"
-// knex('users').where('name', 'Diego').select('*')
+app.use(errors());
 
 app.listen(3333);
